942d4154b8e251f1992a7fbd34ef830ea4bc0acc
revsic <revsic99@gmail.com>
2017-01-30 02:15:20 +0900

    Delete old Branch Tracer

--- BranchTracer/BranchTracer/stdafx.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-
-#define DBG(i) printf("dbg event : %p\n", i);
--- BranchTracer/BrancherDll/BrancherDll.h
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-
-#include <TlHelp32.h>
-#include <Psapi.h>
-#include <DbgHelp.h>
-
-#define MAX_FILE_PATH 512
-
-long WINAPI BranchHandler(PEXCEPTION_POINTERS ExceptionInfo);
