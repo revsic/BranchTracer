6ceb67835d8780a97f8e85bb96caed99cc70ed64
revsic <revsic99@gmail.com>
2019-07-31 02:49:26 +0900

    dllmain: Add annotation

--- Brancher/Brancher/dllmain.cpp
+++ Brancher/Brancher/dllmain.cpp
@@ -1,4 +1,3 @@
-// dllmain.cpp: DLL 응용 프로그램의 진입점을 정의합니다.
 #include "stdafx.h"
 #include "Brancher.h"
 #include "ProcessUtils.h"
@@ -6,19 +5,21 @@
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
-					 )
+                     )
 {
-	switch (ul_reason_for_call)
-	{
-	case DLL_PROCESS_ATTACH:
-		AddVectoredExceptionHandler(1, BranchHandler);
-		SetBreakPointOnEntryPoint();
-		break;
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
-	}
-	return TRUE;
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        // add veh to run branch handler
+        AddVectoredExceptionHandler(1, BranchHandler);
+        // set bp on entry point
+        SetBreakPointOnEntryPoint();
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
 }
 
