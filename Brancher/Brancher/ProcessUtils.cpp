750dc93c5bb6f6f70748748373d3807b18052d6f
revsic <revsic99@gmail.com>
2019-07-31 02:51:28 +0900

    processutils: Replace tab to space

--- Brancher/Brancher/ProcessUtils.cpp
+++ Brancher/Brancher/ProcessUtils.cpp
@@ -19,9 +19,9 @@ int SetBreakPoint(LPVOID lpAddress) {
     DWORD flOldProtect;
     VirtualProtect(lpAddress, 1, PAGE_EXECUTE_READWRITE, &flOldProtect);
 
-	// backup byte
+    // backup byte
     backup = *(BYTE *)lpAddress;
-	// write 0xCC, int 3
+    // write 0xCC, int 3
     *(BYTE *)lpAddress = 0xCC;
 
     return 0;
@@ -40,18 +40,18 @@ int SetBreakPointOnEntryPoint() {
 
     HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, GetCurrentProcessId());
 
-	// get first module
+    // get first module
     Module32FirstW(hSnapshot, &entry);
     CloseHandle(hSnapshot);
 
-	// parse entrypoint
+    // parse entrypoint
     PIMAGE_DOS_HEADER pDosHdr = (PIMAGE_DOS_HEADER)entry.modBaseAddr;
     PIMAGE_NT_HEADERS pNtHdr = ImageNtHeader(pDosHdr);
 
     PIMAGE_OPTIONAL_HEADER pOptHdr = &pNtHdr->OptionalHeader;
     LPVOID lpEntryPoint = entry.modBaseAddr + pOptHdr->AddressOfEntryPoint;
 
-	// set breakpoint on entrypoint
+    // set breakpoint on entrypoint
     SetBreakPoint(lpEntryPoint);
 
     return 0;
@@ -64,18 +64,18 @@ int GetTextSectionAddress(CDWORD *StartOfTextSection, CDWORD *EndOfTextSection) 
 
     HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, GetCurrentProcessId());
 
-	// get first module
+    // get first module
     Module32FirstW(hSnapshot, &entry);
     CloseHandle(hSnapshot);
 
-	// parse entrypoint
+    // parse entrypoint
     PIMAGE_DOS_HEADER pDosHdr = (PIMAGE_DOS_HEADER)entry.modBaseAddr;
     PIMAGE_NT_HEADERS pNtHdr = ImageNtHeader(pDosHdr);
     PIMAGE_SECTION_HEADER pSectionHdr = (PIMAGE_SECTION_HEADER)(pNtHdr + 1);
 
     CDWORD EntryPoint = pNtHdr->OptionalHeader.AddressOfEntryPoint;
 
-	// find section which include the entry point
+    // find section which include the entry point
     DWORD NumberOfSections = pNtHdr->FileHeader.NumberOfSections;
     for (DWORD i = 0; i < NumberOfSections; ++i) {
         CDWORD start = pSectionHdr->VirtualAddress;
@@ -96,7 +96,7 @@ int GetTextSectionAddress(CDWORD *StartOfTextSection, CDWORD *EndOfTextSection) 
 
 // Get module name by address.
 int GetModuleNameByAddr(CDWORD dwAddress, WCHAR *wName, SIZE_T size) {
-	// if symbol uninitialized
+    // if symbol uninitialized
     if (isValueUnset) {
         isValueUnset = false;
 
@@ -112,7 +112,7 @@ int GetModuleNameByAddr(CDWORD dwAddress, WCHAR *wName, SIZE_T size) {
     MODULEENTRY32W entry;
     entry.dwSize = sizeof(entry);
 
-	// iteration module
+    // iteration module
     if (Module32FirstW(hSnapshot, &entry)) {
         do {
             MODULEINFO modinfo;
@@ -121,15 +121,15 @@ int GetModuleNameByAddr(CDWORD dwAddress, WCHAR *wName, SIZE_T size) {
             CDWORD start = (CDWORD)modinfo.lpBaseOfDll;
             CDWORD end = start + modinfo.SizeOfImage;
 
-			// find module which include the given address
+            // find module which include the given address
             if (dwAddress >= start && dwAddress <= end) {
-				// copy module name
+                // copy module name
                 StringCbCopyW(wName, size, entry.szModule);
                 
                 char name[MAX_FILE_PATH];
                 StringCbPrintfA(name, MAX_FILE_PATH, "%ws", wName);
 
-				// load module symbol
+                // load module symbol
                 if (SymLoadModule(hProcess, NULL, name, 0, (CDWORD)modinfo.lpBaseOfDll, 0) || !GetLastError()) {
                     ret = 0;
                 }
@@ -155,7 +155,7 @@ int GetSymbolName(CDWORD called, WCHAR *name, SIZE_T size) {
     pSymbol->SizeOfStruct = sizeof(IMAGEHLP_SYMBOL);
     pSymbol->MaxNameLength = MAX_SYM_NAME;
 
-	// get symbol from address
+    // get symbol from address
     CDWORD dwDisplacement;
     if (SymGetSymFromAddr(hProcess, called, &dwDisplacement, pSymbol)) {
         StringCbPrintfW(name, size, L"%S", pSymbol->Name);
