79ebef15964b05bf9beb12aa56be937e97b71f88
revsic <revsic99@gmail.com>
2019-07-31 02:55:53 +0900

    brancher: Add annotation

--- Brancher/Brancher/Brancher.cpp
+++ Brancher/Brancher/Brancher.cpp
@@ -1,6 +1,3 @@
-// Brancher.cpp : DLL 응용 프로그램을 위해 내보낸 함수를 정의합니다.
-//
-
 #include "stdafx.h"
 #include "Brancher.h"
 #include "ProcessUtils.h"
@@ -11,87 +8,107 @@ HANDLE hStdOutput = NULL;
 CDWORD StartOfTextSection = NULL;
 CDWORD EndOfTextSection = NULL;
 
+// Branch tracer.
 long WINAPI BranchHandler(PEXCEPTION_POINTERS ExceptionInfo) {
-	PEXCEPTION_RECORD record = ExceptionInfo->ExceptionRecord;
-	PCONTEXT context = ExceptionInfo->ContextRecord;
+    PEXCEPTION_RECORD record = ExceptionInfo->ExceptionRecord;
+    PCONTEXT context = ExceptionInfo->ContextRecord;
 
-	if (record->ExceptionCode == EXCEPTION_BREAKPOINT) {
-		BackupBreakPoint(record->ExceptionAddress);
-	}
+    // if break point trigged
+    if (record->ExceptionCode == EXCEPTION_BREAKPOINT) {
+        BackupBreakPoint(record->ExceptionAddress);
+    }
 
-	if (record->ExceptionCode == EXCEPTION_BREAKPOINT
-		|| record->ExceptionCode == EXCEPTION_SINGLE_STEP)
-	{
-		if (isAddrUnset) {
-			isAddrUnset = false;
+    // if debugging event trigged
+    if (record->ExceptionCode == EXCEPTION_BREAKPOINT
+        || record->ExceptionCode == EXCEPTION_SINGLE_STEP)
+    {
+        // if log file uninitialized
+        if (isAddrUnset) {
+            isAddrUnset = false;
 
-			hStdOutput = CreateFileW(L"C:\\dbg\\log.txt", GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-			GetTextSectionAddress(&StartOfTextSection, &EndOfTextSection);
-		}
+            hStdOutput = CreateFileW(L"C:\\dbg\\log.txt", GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+            GetTextSectionAddress(&StartOfTextSection, &EndOfTextSection);
+        }
 
-		bool isBreakPointSet = false;
-		BYTE *opc = (BYTE *)record->ExceptionAddress;
+        bool isBreakPointSet = false;
+        BYTE *opc = (BYTE *)record->ExceptionAddress;
 
-		if (opc[0] == 0xFF) {
-			BYTE Reg = (opc[1] >> 0x3) & 0x7;
-			if (Reg >= 2 && Reg <= 5) { 
-				// Binary 010(near call) or 011(far call) 
-				//        100(near jmp)  or 101(far jmp)
+        // if 0xFF branch operation
+        if (opc[0] == 0xFF) {
+            BYTE Reg = (opc[1] >> 0x3) & 0x7;
+            if (Reg >= 2 && Reg <= 5) { 
+                // Binary 010(near call) or 011(far call) 
+                //        100(near jmp)  or 101(far jmp)
 
-				LPVOID next;
-				CDWORD called = GetBranchingAddress(opc, context, &next);
+                // get destination address
+                LPVOID next;
+                CDWORD called = GetBranchingAddress(opc, context, &next);
 
-				if (called >= EndOfTextSection || called <= StartOfTextSection) {
-					WCHAR log[MAX_LOG_SIZE];
-					WCHAR wModuleName[MAX_FILE_PATH];
-					if (!GetModuleNameByAddr(called, wModuleName, MAX_FILE_PATH)) {
-						WCHAR wSymbolName[MAX_SYM_NAME];
-						if (!GetSymbolName(called, wSymbolName, MAX_SYM_NAME)) {
-							StringCbPrintfW(log, 
-											MAX_LOG_SIZE, 
-											L"+%p,%p,%s,%s\r\n", 
-											record->ExceptionAddress, 
-											called, 
-											wModuleName, 
-											wSymbolName);
-						}
-						else {
-							StringCbPrintfW(log,
-											MAX_LOG_SIZE,
-											L"+%p,%p,%s\r\n",
-											record->ExceptionAddress,
-											called,
-											wModuleName);
-						}
-					}
-					else {
-						StringCbPrintfW(log, MAX_LOG_SIZE, L"+%p,%p,,\r\n", record->ExceptionAddress, called);
-					}
+                // if address out of text section
+                if (called >= EndOfTextSection || called <= StartOfTextSection) {
+                    WCHAR log[MAX_LOG_SIZE];
+                    WCHAR wModuleName[MAX_FILE_PATH];
+                    // get module name where destination address exist
+                    if (!GetModuleNameByAddr(called, wModuleName, MAX_FILE_PATH)) {
+                        WCHAR wSymbolName[MAX_SYM_NAME];
+                        // get symbol name of destination address
+                        if (!GetSymbolName(called, wSymbolName, MAX_SYM_NAME)) {
+                            // write log
+                            StringCbPrintfW(log, 
+                                            MAX_LOG_SIZE, 
+                                            L"+%p,%p,%s,%s\r\n", 
+                                            record->ExceptionAddress, 
+                                            called, 
+                                            wModuleName, 
+                                            wSymbolName);
+                        }
+                        else {
+                            // if symbol doesn't exist
+                            StringCbPrintfW(log,
+                                            MAX_LOG_SIZE,
+                                            L"+%p,%p,%s\r\n",
+                                            record->ExceptionAddress,
+                                            called,
+                                            wModuleName);
+                        }
+                    }
+                    else {
+                        // if module name doesn't exist
+                        StringCbPrintfW(log, MAX_LOG_SIZE, L"+%p,%p,,\r\n", record->ExceptionAddress, called);
+                    }
 
-					DWORD written;
-					WriteFile(hStdOutput, log, (DWORD)wcslen(log) * sizeof(WCHAR), &written, NULL);
+                    // write log message
+                    DWORD written;
+                    WriteFile(hStdOutput, log, (DWORD)wcslen(log) * sizeof(WCHAR), &written, NULL);
 
-					SetBreakPoint(next);
-					isBreakPointSet = true;
-				}
-			}
-		}
-		else if (opc[0] == 0xE8) {
-			CDWORD called = context->RegisterIp + *(long *)&opc[1] + 5;
+                    // set bp on return address
+                    SetBreakPoint(next);
+                    isBreakPointSet = true;
+                }
+            }
+        }
+        // if 0xE8 branching operation
+        else if (opc[0] == 0xE8) {
+            // get destination address
+            CDWORD called = context->RegisterIp + *(long *)&opc[1] + 5;
 
-			DWORD written;
-			WCHAR log[MAX_LOG_SIZE];
-			StringCbPrintfW(log, MAX_LOG_SIZE, L"+%p,%p,,\r\n", record->ExceptionAddress, called);
-			WriteFile(hStdOutput, log, (DWORD)wcslen(log) * sizeof(WCHAR), &written, NULL);
-		}
+            DWORD written;
+            WCHAR log[MAX_LOG_SIZE];
+            // write log message
+            StringCbPrintfW(log, MAX_LOG_SIZE, L"+%p,%p,,\r\n", record->ExceptionAddress, called);
+            WriteFile(hStdOutput, log, (DWORD)wcslen(log) * sizeof(WCHAR), &written, NULL);
+        }
 
-		if (!isBreakPointSet) {
-			SetSingleStepContext(context);
-		}
+        // set trap flag for sigle step exception
+        if (!isBreakPointSet) {
+            SetSingleStepContext(context);
+        }
 
-		return EXCEPTION_CONTINUE_EXECUTION;
-	}
-	else {
-		return EXCEPTION_CONTINUE_SEARCH;
-	}
+        // continue process without passing exception
+        return EXCEPTION_CONTINUE_EXECUTION;
+    }
+    else {
+        // pass exception to process
+        return EXCEPTION_CONTINUE_SEARCH;
+    }
 }
