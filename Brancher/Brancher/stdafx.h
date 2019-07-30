5c2c561379d2e2e062e535f93d3d25abcbe03a93
revsic <revsic99@gmail.com>
2019-07-31 02:34:26 +0900

    stdafx: Add annotation

--- Brancher/Brancher/stdafx.h
+++ Brancher/Brancher/stdafx.h
@@ -1,23 +1,20 @@
-// stdafx.h : 자주 사용하지만 자주 변경되지는 않는
-// 표준 시스템 포함 파일 또는 프로젝트 관련 포함 파일이
-// 들어 있는 포함 파일입니다.
-//
-
 #pragma once
 
 #include "targetver.h"
 
-#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용은 Windows 헤더에서 제외합니다.
-// Windows 헤더 파일:
+#define WIN32_LEAN_AND_MEAN 
+
 #include <windows.h>
 #include <strsafe.h>
 
 #define MAX_FILE_PATH 512
 #define MAX_LOG_SIZE 256
 
+// if x64 environment
 #ifdef _WIN64
 typedef DWORD64 CDWORD;
 
+// declare register with rax-family
 #define RegisterAx Rax
 #define RegisterCx Rcx
 #define RegisterDx Rdx
@@ -28,9 +25,11 @@ typedef DWORD64 CDWORD;
 #define RegisterDi Rdi
 #define RegisterIp Rip
 
+// if x86 environment
 #else
 typedef DWORD CDWORD;
 
+// declare register with eax-familty
 #define RegisterAx Eax
 #define RegisterCx Ecx
 #define RegisterDx Edx
@@ -42,5 +41,3 @@ typedef DWORD CDWORD;
 #define RegisterIp Eip
 
 #endif
-
-// TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
