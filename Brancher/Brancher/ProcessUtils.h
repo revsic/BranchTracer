289db653db559dfe6ee8c3ae32c156ede996a92c
revsic <revsic99@gmail.com>
2019-07-31 02:42:27 +0900

    processutils: Add annotation

--- Brancher/Brancher/ProcessUtils.h
+++ Brancher/Brancher/ProcessUtils.h
@@ -4,13 +4,20 @@
 #include <Psapi.h>
 #include <DbgHelp.h>
 
+// Set trap flag to make EXCEPTION_SINGLE_STEP.
 int SetSingleStepContext(PCONTEXT context);
 
+// Set software breakpoint with 0xCC.
 int SetBreakPoint(LPVOID lpAddress);
+// Recover current byte which replaced with 0xCC.
 int BackupBreakPoint(LPVOID lpAddress);
 
+// Set software breakpoint on entry point.
 int SetBreakPointOnEntryPoint();
+// Get start and end address of text section.
 int GetTextSectionAddress(CDWORD *StartOfTextSection, CDWORD *EndOfTextSection);
 
+// Get module name by address.
 int GetModuleNameByAddr(CDWORD dwAddress, WCHAR *wName, SIZE_T size);
+// Get symbol name by address.
 int GetSymbolName(CDWORD called, WCHAR *name, SIZE_T size);
