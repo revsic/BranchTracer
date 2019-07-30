ed36bfaa84ff5a573782dd8e58c02e1bf7f0ab33
revsic <revsic99@gmail.com>
2019-07-31 02:35:40 +0900

    rawlevelhelper: Add annotation

--- Brancher/Brancher/RawlevelHelper.h
+++ Brancher/Brancher/RawlevelHelper.h
@@ -1,9 +1,12 @@
 #include "stdafx.h"
 
+// Result of SIB parser
 struct SIBParseResult {
 	BYTE *opc;
 	CDWORD called;
 };
 
+// Get destination address of branching operation.
 CDWORD GetBranchingAddress(BYTE *opc, PCONTEXT context, LPVOID *next);
+// Parse SIB from given context.
 CDWORD SIBParser(BYTE* opc, PCONTEXT context, SIBParseResult *result);
